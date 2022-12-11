d18b8ea56c245e73b50c4803ed027fea683a4e87
Amjad Khan <[toakhan@hotmail.com]>
2022-12-11 10:38:35 +0000

    added shopping example

--- Shopping Example/shopping.js
+++ Shopping Example/shopping.js
@@ -1,1 +1,8 @@
 // Code here for Compulsory Task 2
+let item1=prompt("What is first item to shop?");
+console.log(item1);
+let item2=prompt("What is second item to shop?");
+console.log(item2);
+let item3=prompt("What is third item to shop?");
+console.log(item3);
+console.log("My Shopping List: " , item1 , item2 , item3 ,); 
