d88f7f8026fa60974e757f0fdac1fb6075aa719b
Amjad Khan <[toakhan@hotmail.com]>
2022-12-11 08:33:46 +0000

    greetings.js file modified in vs code

--- Greeting Example/greeting.js
+++ Greeting Example/greeting.js
@@ -1,1 +1,8 @@
 // Code here for Compulsory Task 1
+let userName = prompt("What is your name?");
+console.log("Hi there "+userName);
+
+let userAge = prompt("What is your age?");
+console.log("Your age is " + userAge );
+
+console.log("\nIts nice to meet you!");
